ec840da66120f0e484a6e69450ff74e2ae3c819a
JAM2199562 <JAM2199562@maricopa.edu>
2025-04-20 18:17:04 +0800

    chore: 将编译后的文件添加到 .gitignore

--- lib/cli/account.d.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from 'commander';
-export declare const privateKeysCommand: Command;
-export declare const mnemonicToAccountCommand: Command;
-export declare const generateMnemonicCommand: Command;
-export declare const signPsbt: Command;
